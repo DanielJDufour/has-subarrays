c694c55678b3a2e468756dd96b0291fc7f72e526
DanielJDufour <daniel.j.dufour@gmail.com>
2020-11-15 18:31:03 -0500

    updated

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # has-subarrays
 Quickly Determine if an Array contains Subarrays
+
+# install
+```bash
+npm install has-subarrays
+```
+
+# usage
+```javascript
+const hasSubArrays = require("has-subarrays");
+
+const numbers_one_to_ten = [1,2,3,4,5,6,7,8,9,10];
+
+const { result } = hasSubArrays({
+    data: numbers_one_to_ten,
+    debug: true, // turn on for extra logging
+    subarrays: {
+        'one then two': [1, 2],
+        'three then four': [3, 4]
+    }
+});
+// result is true
+```
+
+# setting a threshold
+You can specify a threshold, the number of unique matches after which it will return true.
+If you don't specify a threshold, it will assume all subarrays must be found.
+```javascript
+const { result } = hasSubArrays({
+    data: numbers_one_to_ten,
+    debug: true, // turn on for extra logging
+    subarrays: {
+        'one then two': [1, 2],
+        'three then four': [3, 4]
+    },
+    threshold: 1 // only one of the two subarrays must be found
+});
+// result is true
+```
--- has-subarrays.js
+++ has-subarrays.js
@@ -1,63 +1,70 @@
-function len (arr) {
-    if (arr.length !== undefined) return arr.length;
-    if (arr.byteLength !== undefined) return arr.byteLength;
+function len(arr) {
+  if (arr.length !== undefined) return arr.length;
+  if (arr.byteLength !== undefined) return arr.byteLength;
 }
-function hasSubArrays ({ data, debug=false, targets, start, end=null, reverse=false, threshold=null }) {
+function hasSubArrays({
+  data,
+  debug = false,
+  subarrays,
+  start,
+  end = null,
+  reverse = false,
+  threshold = null
+}) {
+  if (!data) throw new Error("[has-subarrays] you must specify data");
 
-    if (!data) throw new Error("[has-subarrays] you must specify data");
+  if (start === undefined || start === null) start = 0;
+  if (end === undefined || end === null) end = len(data) - 1;
+  if (threshold === undefined || threshold === null)
+    threshold = Object.keys(subarrays).length;
 
-    if (start === undefined || start === null) start = 0;
-    if (end === undefined || end === null) end = len(data) - 1;
-    if (threshold === undefined || threshold === null) threshold = Object.keys(targets).length;
-    
-    // create indexed search tree
-    const tree = {};
+  // create indexed search tree
+  const tree = {};
 
-    for (let name in targets) {
-        // if (debug) console.log("[has-subarrays] name:", name);
-        const sequence = targets[name];
-        let branch = tree;
-        for (let i = 0; i < sequence.length - 1; i++) {
-            const byte = sequence[i];
-            if (!branch[byte]) branch[byte] = {};
-            branch = branch[byte];
-        }
-        branch[sequence[sequence.length - 1]] = name;
-        // console.log("tree (after branch):", tree);
+  for (let name in subarrays) {
+    // if (debug) console.log("[has-subarrays] name:", name);
+    const sequence = subarrays[name];
+    let branch = tree;
+    for (let i = 0; i < sequence.length - 1; i++) {
+      const byte = sequence[i];
+      if (!branch[byte]) branch[byte] = {};
+      branch = branch[byte];
     }
-    if (debug) console.log("tree:", tree);
+    branch[sequence[sequence.length - 1]] = name;
+    // console.log("tree (after branch):", tree);
+  }
+  if (debug) console.log("tree:", tree);
 
-    // search
-    const found = new Set();
-    for (let i = start; i <= end; i++) {
-        if (debug) console.log("i:", i);
-        let branch = tree
-        // need to add in pruning parents
-        for (let ii = i; ii <= end; ii++) {
-            if (debug) console.log("ii:", ii);
-            const next = data[ii];
-            const type = typeof branch[next];
-            if (debug) console.log("type:", type);
-            if (type === "undefined") {
-                break;
-            } else if (type === "object") {
-                branch = branch[next];
-                if (debug) console.log("found branch object", branch);
-            } else if (type === "string") {
-                if (debug) console.log("found end:", branch);
-                found.add(branch[next]);
-                delete branch[next];
-                if (found.size === threshold) {
-                    return { found: threshold, result: true, threshold, tree };
-                }
-            }
+  // search
+  const found = new Set();
+  for (let i = start; i <= end; i++) {
+    if (debug) console.log("i:", i);
+    let branch = tree;
+    // need to add in pruning parents
+    for (let ii = i; ii <= end; ii++) {
+      if (debug) console.log("ii:", ii);
+      const next = data[ii];
+      const type = typeof branch[next];
+      if (debug) console.log("type:", type);
+      if (type === "undefined") {
+        break;
+      } else if (type === "object") {
+        branch = branch[next];
+        if (debug) console.log("found branch object", branch);
+      } else if (type === "string") {
+        if (debug) console.log("found end:", branch);
+        found.add(branch[next]);
+        delete branch[next];
+        if (found.size === threshold) {
+          return { found: threshold, result: true, threshold, tree };
         }
+      }
     }
-    
-    
-    return { tree };
-};
+  }
 
-if (typeof module === 'object') module.exports = hasSubArrays;
-if (typeof window !== 'undefined') window.hasSubArrays = hasSubArrays;
-if (typeof self !== 'undefined') self.hasSubArrays = hasSubArrays;
+  return { tree };
+}
+
+if (typeof module === "object") module.exports = hasSubArrays;
+if (typeof window !== "undefined") window.hasSubArrays = hasSubArrays;
+if (typeof self !== "undefined") self.hasSubArrays = hasSubArrays;
