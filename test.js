53bc55f24050dc71b6a5bf841b2cc8e654972150
DanielJDufour <daniel.j.dufour@gmail.com>
2020-11-15 18:22:51 -0500

    prototype

---
+++ test.js
@@ -0,0 +1,56 @@
+const test = require("ava");
+const hasSubArrays = require("./has-subarrays");
+
+const nums = [1,2,3,4,5,6,7,8,9,10];
+
+test("has single sequence", t => {
+    const arr = Uint8Array.from(nums);
+    const { result, threshold, tree } = hasSubArrays({
+        data: arr,
+        debug: false,
+        targets: {
+            'one': [1],
+            'two': [2]
+        }
+    });
+    t.deepEqual(tree, { });
+    t.true(result);
+    t.is(threshold, 2);
+});
+
+
+test("has double sequence", t => {
+    const arr = Uint8Array.from(nums);
+    const { result, threshold, tree } = hasSubArrays({
+        data: arr,
+        debug: false,
+        targets: {
+            'one-two': [1, 2],
+            'three-four': [3, 4]
+        }
+    });
+    t.deepEqual(tree, { 1: { }, 3: { } });
+    t.true(result);
+    t.is(threshold, 2);
+});
+
+test("has threshold set", t => {
+    const arr = Uint8Array.from(nums);
+    const { result, threshold, tree } = hasSubArrays({
+        data: arr,
+        debug: false,
+        targets: {
+            'five-six-seven': [5, 6, 7],
+            'six-seven-eight': [6, 7, 8],
+            'one-seven-nine': [1, 7, 9]
+        },
+        threshold: 2
+    });
+    t.deepEqual(tree, {
+        1: { 7: { 9: 'one-seven-nine' } },
+        5: { 6: {} },
+        6: { 7: {} }
+    });
+    t.true(result);
+    t.is(threshold, 2);
+});
